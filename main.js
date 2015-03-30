2d4d062e6e52acd4aa837b4b51eb3ad7a7bcf0f1
TROY <troyporter@Troys-MacBook-Air.local>
2015-03-30 19:46:29 +0000

    making TrominoC an option again
    i had made them all “I”s to test for that completion bug

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ var main = {
 	inactives: [],
 	
 	createTromino: function() {
-		this.active = Math.random()<0.5? new TrominoI():new TrominoI();
+		this.active = Math.random()<0.5? new TrominoI():new TrominoC();
 	},
 	
 	dropTromino: function() {
